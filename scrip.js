a35db9bf1b4e5c43659d6563dea22658c46901a0
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2021-10-12 21:52:27 +0300

    Done leson 7,8,9

--- scrip.js
+++ scrip.js
@@ -11,5 +11,29 @@
 // let isBoss = confirm("Who is Boss?");
 // alert(isBoss);
 
-let userName = prompt('What is your name?', );
-alert(userName);
+// let userName = prompt('What is your name?', );
+// alert(userName);
+
+// Базовые операторы математики
+// преоброзование типов 
+// "" + 1 + 0 // 10
+// "" - 1 + 0 // -1
+// true + false // 1
+// 6 / "3" // 2
+// "2" * "3" // 6
+// 4 + 5 + "px" // 9px
+// "$" + 4 + 5 // $45
+// "4" - 2 // 2
+// "4px" - 2 // NaN
+// 7 / 0 // infinity 
+// "  -9  " + 5 //-9 5
+// "  -9  " - 5 // -14
+// null + 1 // 1
+// undefined + 1 // NaN
+// " \t \n" - 2 // -2
+
+// Задача 
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+
+// alert(+a + +b); // 12
