7ff60f5b052438d1083f303218d73e57e7b1edb0
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2021-10-14 16:26:46 +0300

    logical operations

--- scrip.js
+++ scrip.js
@@ -35,3 +35,71 @@
 // let b = prompt("Второе число?", 2);
 
 // alert(+a + +b); // 12 
+
+// Ver1.2 
+// Task name JavaScript
+// let nameJavaScript = prompt ('Какое официально название JavaScript?', '');
+
+// if (nameJavaScript == "ECMAScript") {
+//     alert ('Правильно!');
+// } else {
+//     alert ('Не знаете? "ECMAScript"');
+// }
+
+// Task знак числа
+// let number = prompt ('Write number', '');
+
+// if (number > 0) {
+//     alert (1);
+// } else if (number < 0){
+//     alert(-1);
+// } else {
+//     alert(0);
+// }
+
+//  let result;
+
+// if (a + b < 4) ? 'Мало' : 'Много';
+ 
+
+// let message;
+
+// let message (login == 'Сотрудник') ? 'Привет':
+//   (login == 'Директор') ? 'Здравствуйте':
+//  (login == '') ? 'Нет логина':
+//   '';
+
+// Проверка значений из диапазона
+// let age = 23;
+
+// if (age >=14 && age <=90) {
+//     alert ("All good");
+// } else {
+//         alert("Try again");
+//     }
+
+//Проверка значений вне диапазона
+// let age = 23;
+
+// if (!(age >= 14 && age <= 90))
+// if (age <= 14 || age >= 90)
+
+// Task проверка логина
+// let userName = prompt ('Who are you?', '');
+//  if (userName == 'Admin') {
+//     let pass = prompt('Pass?', '')
+//     if ( pass== 'I owner') {
+//         alert('Hello');
+//     } else if( pass =='' || pass == null) {
+//         alert('cancellation');
+//     }else {
+//         alert('Sorry password is wrong')
+//     }
+//  }
+//  else if (userName =='' || userName == null) {
+//      alert('cancellation');
+//  } else {
+//      alert('Sorry you are not my owner');
+//  }
+
+
