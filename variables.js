9aaab5675afc7dc4b457af8c62c2f0a05cec6f0c
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2021-10-16 11:35:16 +0300

    separate variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+ //"use strict";
+ //alert( "Я JavaScript!" );
+// let name = 'Джон';
+// let admin = name;
+// alert (admin);
+// let ourPlanetName = "Earth";
+// let userName = name;
+//  let age = prompt('How oll are you?', chouse);
+// alert('Тебе ${age} лет!`);
+// let isBoss = confirm("Who is Boss?");
+// alert(isBoss);
+// let userName = prompt('What is your name?', );
+// alert(userName);
