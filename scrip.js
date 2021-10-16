2e6000712935304636dedd98f0cf1c31d3bd16b1
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2021-10-16 11:47:23 +0300

    conditional-branching-if

--- scrip.js
+++ scrip.js
@@ -1,32 +1,3 @@
-// if (nameJavaScript == "ECMAScript") {
-//     alert ('Правильно!');
-// } else {
-//     alert ('Не знаете? "ECMAScript"');
-// }
-
-// Task знак числа
-// let number = prompt ('Write number', '');
-
-// if (number > 0) {
-//     alert (1);
-// } else if (number < 0){
-//     alert(-1);
-// } else {
-//     alert(0);
-// }
-
-//  let result;
-
-// if (a + b < 4) ? 'Мало' : 'Много';
- 
-
-// let message;
-
-// let message (login == 'Сотрудник') ? 'Привет':
-//   (login == 'Директор') ? 'Здравствуйте':
-//  (login == '') ? 'Нет логина':
-//   '';
-
 // Проверка значений из диапазона
 // let age = 23;
 
