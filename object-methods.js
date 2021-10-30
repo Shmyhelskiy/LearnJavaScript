bb9cac74bcf4c2600452ea074cb8f6c4eb98b7c0
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2021-10-30 14:37:37 +0300

    end-object-methods

---
+++ object-methods.js
@@ -0,0 +1,37 @@
+//Task calculator
+// let calculator = {
+//     sum () {
+//         return this.a + this.b ;
+//     },
+
+//     mul () {
+//         return this.a * this.b;
+//     },
+    
+//     read () {
+//         this.a = +prompt('write a', 0);
+//         this.b = +prompt('write b', 0);
+//     },
+// };
+
+
+// calculator.read();
+// alert( calculator.sum() );
+// alert( calculator.mul() );
+
+//Task more call
+// let ladder = {
+//     step: 0,
+//     up() {
+//       this.step++;
+//       return this
+//     },
+//     down() {
+//       this.step--;
+//       return this
+//     },
+//     showStep: function() { // показывает текущую ступеньку
+//       alert( this.step );
+//       return this
+//     }
+//   };
