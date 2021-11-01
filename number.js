6dae7c2064861e8abcf6ba02b81f1dfd1d6f26a4
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2021-11-01 16:47:11 +0200

    number-task

---
+++ number.js
@@ -0,0 +1,32 @@
+//Task sum number
+// let a = +prompt('Write number a', '');
+// let b = +prompt('Write number b', '');
+
+// alert(a + b);
+
+//Task Entering a numeric value
+
+// function readNumber () {
+//     let num;
+
+//    do {
+//      num = prompt('Write number', 0);
+//    } while ( !isFinite(num) );
+
+//    if ( num === null || num === ' ')  return null; 
+
+ 
+//    return +num
+// };
+
+// alert (readNumber());
+
+
+// Task random min max
+
+// function random (min, max) {
+//     let num = Math.random() * (max - min + 1) + min;
+//     return Math.floor(num);
+// }
+
+// alert(random(1, 3));
